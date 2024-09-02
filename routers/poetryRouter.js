529206f6a4b59ce7fa6fe0b035ac27c9e5046c92
khoirulhudaa <developervalclass@gmail.com>
2024-09-02 13:28:27 +0700

    feat: add router

--- routers/poetryRouter.js
+++ routers/poetryRouter.js
@@ -4,7 +4,7 @@ const poetryController = require('../controllers/poetryController')
 
 router.post('/:user_id/:book_id', poetryController.createPoetry)
 router.get('/', poetryController.getAllPoetry)
-router.get('/:authorId', poetryController.getAllPoetryByAuthorId)
+router.get('/all/:authorId', poetryController.getAllPoetryByAuthorId)
 router.get('/:book_id', poetryController.getAllPoetryByBookId)
 router.put('/update/:poetry_id', poetryController.updatePoetry)
 router.delete('/remove/:poetry_id', poetryController.removePoetry)
